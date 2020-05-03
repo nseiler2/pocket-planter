eedfe9dfd0c129f00759794a7276d0b2cac7fb8f
Nathan <nseiler2@illinois.edu>
2020-05-02 22:44:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pocketplanter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pocketplanter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
